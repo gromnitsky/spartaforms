73f86d3cd73c5cff5037fd1f8a18924e1c5013a0
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2025-01-18 20:14:55 +0200

    mkschema.js: add integers

--- mkschema.js
+++ mkschema.js
@@ -10,13 +10,25 @@ function getters(o) {
         .map(([key]) => key)
 }
 
-class EString {
-    constructor(name) {
+class E {
+    constructor(name, type) {
         this.name = name
+        this.type = type
         this.required = false
-        this._pattern
-        this._minLength
-        this._manLength
+    }
+
+    toJSON() {
+        let r = { type: this.type }
+        getters(this).forEach( fn => {
+            if (this[fn] != null) r[fn] = this[fn]
+        })
+        return r
+    }
+}
+
+class EString extends E {
+    constructor(name) {
+        super(name, 'string')
     }
 
     set pattern(v) {
@@ -44,14 +56,36 @@ class EString {
     get pattern() { return this._pattern }
     get minLength() { return this._minLength }
     get maxLength() { return this._maxLength }
+}
 
-    toJSON() {
-        let r = { type: "string" }
-        getters(this).forEach( fn => {
-            if (this[fn] != null) r[fn] = this[fn]
-        })
-        return r
+class EInteger extends E {
+    constructor(name) {
+        super(name, 'integer')
     }
+
+    set minimum(v) {
+        v = parseInt(v) || 0
+        this._minimum = v
+    }
+
+    set maximum(v) {
+        v = parseInt(v)
+        if (isNaN(v)) return
+        this._maximim = v
+    }
+
+    get minimum() { return this._minimum }
+    get maximum() { return this._maximim }
+}
+
+// modifies 'schema'
+function collect_props(nodes, fn_filter, fn_map, schema) {
+    let elements = $(nodes).filter( (idx, v) => fn_filter(idx, v)).toArray()
+    elements = elements.map(fn_map)
+
+    elements.forEach( v => schema.properties[v.name] = v.toJSON())
+    schema.required = schema.required
+        .concat(elements.filter( v => v.required).map( v => v.name))
 }
 
 function err(...s) {
@@ -63,30 +97,38 @@ if (process.argv.length !== 4) err('Usage: mkschema.js form.html css-selector')
 
 let $ = cheerio.loadBuffer(fs.readFileSync(process.argv[2]))
 let form = $(process.argv[3])
-if (form.length > 1) err('more > 1 forms match')
+if (form.length > 1) err('> 1 forms match')
 
 let nodes = $(form).find('*[name]')
+let schema = {
+    type: 'object',
+    properties: {},
+    required: []
+}
 
-let text = $(nodes).filter( (idx, v) => {
+/* <input type="text"> */
+collect_props(nodes, (idx, v) => {
     return /^text|search$/.test($(v).attr('type')) && v.name === 'input'
-}).toArray()
-
-text = text.map( v => {
+}, v => {
     let el = new EString($(v).attr("name"))
     el.required = $(v).attr("required") != null
     el.pattern = $(v).attr("pattern")
     el.minLength = $(v).attr("minlength")
     el.maxLength = $(v).attr("maxlength")
     return el
-})
+}, schema)
 
-let r = {
-    type: 'object',
-    properties: {},
-    required: []
-}
+/* <input type="number"> */
+collect_props(nodes, (idx, v) => {
+    return $(v).attr('type') === 'number' && v.name === 'input'
+}, v => {
+    let el = new EInteger($(v).attr("name"))
+    el.required = $(v).attr("required") != null
+    el.minimum= $(v).attr("min")
+    el.maximum= $(v).attr("max")
+    return el
+}, schema)
 
-text.forEach( v => r.properties[v.name] = v.toJSON())
-r.required = r.required.concat(text.filter( v => v.required).map( v => v.name))
 
-process.stdout.write(JSON.stringify(r, null, 2))
+/* fin */
+process.stdout.write(JSON.stringify(schema, null, 2))
