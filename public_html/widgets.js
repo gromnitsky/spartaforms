381f9f1960bc217e718dc3109618ea4b296b8e42
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2025-01-21 14:50:43 +0200

    spartaforms-slider: work w/ fieldset

--- public_html/widgets.js
+++ public_html/widgets.js
@@ -1,14 +1,13 @@
-customElements.define('spartaforms-slider', class extends HTMLElement {
+class SpartaForms_Slider extends HTMLElement {
     static formAssociated = true
 
     constructor() {
         super()
 
-        this.name     = this.getAttribute('name')
-        this.value    = this.getAttribute('value')
-        this.min      = this.getAttribute('min')
-        this.max      = this.getAttribute('max')
-        this.fieldset = this.getAttribute('fieldset')
+        let name      = this.getAttribute('name')
+        let value     = this.getAttribute('value')
+        let min       = this.getAttribute('min')
+        let max       = this.getAttribute('max')
 
         let s = this.attachShadow({mode: 'open'})
         s.innerHTML = `
@@ -42,18 +41,24 @@ customElements.define('spartaforms-slider', class extends HTMLElement {
 
 <div id="container">
   <div id="input">
-    <input type="range" name="${this.name}" list="values"
-           min="${this.min}" max="${this.max}" value="${this.value}">
+    <input type="range" name="${name}" list="values"
+           min="${min}" max="${max}" value="${value}">
   </div>
   <slot id="desc"></slot>
 </div>
 `
+        this._internals = this.attachInternals()
     }
+
+    get form() { return this._internals.form }
+    get name() { return this.getAttribute('name') }
+    get type() { return this.localName }
 
     connectedCallback() {
         let doc = this.shadowRoot
         let spans = doc.querySelector('#desc').assignedElements()
 
+        // make <datalist> for out <input>
         let datalist = document.createElement('datalist')
         datalist.id = 'values'
         spans.forEach( span => {
@@ -63,8 +68,9 @@ customElements.define('spartaforms-slider', class extends HTMLElement {
         })
         doc.querySelector('#container').appendChild(datalist)
 
+        // handle clicks on light dom <span>s
         let input = doc.querySelector('#input input')
-        let fieldset = document.querySelector(this.fieldset)
+        let fieldset = this.form.querySelector('fieldset')
         let fn = node => node.onclick = span => {
             if (fieldset.disabled) return
             input.value = span.currentTarget.dataset.value
@@ -72,19 +78,24 @@ customElements.define('spartaforms-slider', class extends HTMLElement {
         }
         spans.forEach(fn)
 
-        this.internals = this.attachInternals()
-        this.internals.setFormValue(input.value) // send initial value
+        // inform <form> of our shadow dom <input>
+        this._internals.setFormValue(input.value) // send initial value
         input.addEventListener("change", () => {
-            this.internals.setFormValue(input.value)
+            this._internals.setFormValue(input.value)
         })
     }
 
     static observedAttributes = ['value']
     attributeChangedCallback(name, old_value, new_value) {
-        if (name === 'value') {
-            let input = this.shadowRoot.querySelector('#input input')
-            input.value = new_value
-            input.dispatchEvent(new Event('change'))
-        }
+        let input = this.shadowRoot.querySelector('#input input')
+        input.value = new_value
+        input.dispatchEvent(new Event('change'))
     }
-})
+
+    formDisabledCallback(disabled) {
+        let input = this.shadowRoot.querySelector('#input input')
+        input.disabled = disabled
+    }
+}
+
+customElements.define('spartaforms-slider', SpartaForms_Slider)
